45ee52a75841877c6a48e8ff417f9e0ee4835e4c
AyseKoklu <rukoklu32@gmail.com>
2022-04-27 17:50:32 +0100

    Initial commit

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,80 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1- Set up the "browser driver"
+        WebDriverManager.chromedriver().setup();
+
+        // 2- Create instance of the Selenium WebDriver
+        // this is the line that opens the empty browser
+        WebDriver driver = new ChromeDriver();
+
+        // this line will maximise the browser size
+        driver.manage().window().maximize();
+
+        driver.manage().window().fullscreen();
+
+        // 3- Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        // get the title of the page
+        String currentTitle = driver.getTitle();
+        System.out.println("Current Title = " + currentTitle);
+
+        // Get the current URL using Selenium
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL = " + currentURL);
+
+        // Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use Selenium to navigate back
+        driver.navigate().back();
+
+        // Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use Selenium to navigate forward
+        driver.navigate().forward();
+
+        // Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use Selenium to navigate refresh
+        driver.navigate().refresh();
+
+        // Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use navigate().to:
+        driver.navigate().to("https://www.google.com");
+
+        // get the title of the page
+        // System.out.println(driver.getTitle());
+        // or
+        // System.out.println("driver.getTitle() = " + driver.getTitle());
+        // or
+        currentTitle = driver.getTitle();
+        System.out.println("Current Title = " + currentTitle);
+
+        // Get the current URL using Selenium
+        currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL = " + currentURL);
+
+        // this will close the currently opened window
+        driver.close();
+
+        // this will close all the opened windows / kills the session
+        driver.quit(); // after this we will get NoSuchSessionException
+
+
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1_YahooTitleVerification {
+
+    public static void main(String[] args) {
+
+        // TC #1: Yahoo Title Verification
+        // do set up for browser driver
+        WebDriverManager.chromedriver().setup();
+
+        // 1. Open Chrome browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Go to https://www.yahoo.com
+        driver.get("https://www.yahoo.com");
+
+        // 3. Verify title:
+        // Expected: Yahoo is part of the Yahoo family of brands
+        String expectedTitle = "Yahoo is part of the Yahoo family of brands";
+
+        // actual title will come from the browser
+        String currentTitle = driver.getTitle();
+
+        System.out.println("Expected Title = " + expectedTitle);
+        System.out.println("Current Title = " + currentTitle);
+
+        if (expectedTitle.equals(currentTitle)){
+            System.out.println("Title is as expected. Verification PASSED!");
+        } else{
+            System.out.println("Title is NOT as expected. Verification FAILED!");
+        }
+
+
+    }
+
+}
