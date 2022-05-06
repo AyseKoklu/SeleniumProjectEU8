95b5919c5367b0c512ba91067a53615df05265f6
AyseKoklu <rukoklu32@gmail.com>
2022-05-06 21:51:42 +0100

    day 02 - Automation

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class T5_getText_getAttribute {
+public class T5_GetText_GetAttribute {
 
     public static void main(String[] args) {
 
---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,13 @@
+package com.cydeo.utilities;
+
+// TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
+public class WebDriverFactory {
+}
