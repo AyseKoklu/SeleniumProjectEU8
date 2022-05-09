2271ca9393d8da1618e641d4aa06a94210d71469
AyseKoklu <rukoklu32@gmail.com>
2022-05-09 01:50:58 +0100

    day 02 - Automation

--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -9,5 +9,36 @@ package com.cydeo.utilities;
 //   - if "firefox" passed --> it will open firefox browser
 // RETURN TYPE: "WebDriver"
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+
+        if (browserType.equalsIgnoreCase("Chrome")){
+
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase("firefox")){
+
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        } else {
+            System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driven = null");
+            return null;
+        }
+
+    }
+
+
+
+
+
+
 }
