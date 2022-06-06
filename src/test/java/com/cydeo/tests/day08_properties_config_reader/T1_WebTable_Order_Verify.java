ecccdc1217a82f6fdaab8caa28beef60cdb2ad62
AyseKoklu <rukoklu32@gmail.com>
2022-06-06 14:30:40 +0100

    day 08

---
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,66 @@
+package com.cydeo.tests.day08_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebTableUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WebTable_Order_Verify {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // 1. Go to: https://practice.cydeo.com/web-tables
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
+
+    @Test
+    public void order_name_verify_test(){
+        // TC #1: Web table practice
+
+        // locate the cell that has Bob Martin text in it
+        WebElement bobName = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
+
+        // 2. Verify Bob’s name is listed as expected.
+        // Expected: “Bob Martin”
+        String expectedName = "Bob Martin";
+        String actualName = bobName.getText();
+
+        Assert.assertTrue(expectedName.equals(actualName));
+
+        // 3. Verify Bob Martin’s order date is as expected.
+        // Expected: 12/31/2021
+        WebElement bobDate = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
+
+        String expectedDate = "12/31/2021";
+        String actualDate = bobDate.getText();
+
+        Assert.assertTrue(expectedDate.equals(actualDate));
+
+    }
+
+    @Test
+    public void test2(){
+        String customerOrderDate1 = WebTableUtils.returnOrderDate(driver,"Alexandra Gray");
+        System.out.println("customerOrderDate1 = " + customerOrderDate1);
+
+        String customerOrderDate2 = WebTableUtils.returnOrderDate(driver,"John Doe");
+        System.out.println("customerOrderDate2 = " + customerOrderDate2);
+    }
+
+    @Test
+    public void test3(){
+        WebTableUtils.orderVerify(driver, "John Doe", "01/08/2021");
+    }
+
+}
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,6 +1,6 @@
 package com.cydeo.utilities;
 
-//In this class only general utility methods that are NOT related to some specific page.
+// In this class only general utility methods that are NOT related to some specific page.
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -9,9 +9,8 @@ import java.util.Set;
 
 public class BrowserUtils {
 
-    //This method will accept int (in seconds) and execute Thread.sleep for given duration
+    // This method will accept int (in seconds) and execute Thread.sleep for given duration
     public static void sleep(int second) {
-
         second *= 1000;
         try {
             Thread.sleep(second);
@@ -19,11 +18,13 @@ public class BrowserUtils {
         }
     }
 
-    // This method accepts 3 arguments.
-    // Arg1: webdriver
-    // Arg2: expectedInUrl : for verify if the url contains given String.
-    // - If condition matches, will break loop.
-    // Arg3: expectedInTitle to be compared against actualTitle
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
@@ -36,6 +37,7 @@ public class BrowserUtils {
             }
         }
 
+        // Assert: Title contains “expectedInTitle”
         String actualTitle = driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
     }
@@ -43,5 +45,6 @@ public class BrowserUtils {
     // This method accepts a String "expectedTitle" and Asserts if it is true
     public static void verifyTitle(WebDriver driver, String expectedTitle) {
         Assert.assertEquals(driver.getTitle(), expectedTitle);
+
     }
 }
