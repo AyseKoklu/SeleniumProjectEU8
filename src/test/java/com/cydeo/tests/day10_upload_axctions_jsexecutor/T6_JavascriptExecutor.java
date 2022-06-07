b8103b64c74b39cc77a7c467684cd11504754dbc
AyseKoklu <rukoklu32@gmail.com>
2022-06-07 19:04:06 +0100

    day 10

--- src/test/java/com/cydeo/tests/day10_upload_axctions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_axctions_jsexecutor/T6_JavascriptExecutor.java
@@ -7,20 +7,25 @@ import org.testng.annotations.Test;
 public class T6_JavascriptExecutor {
 
     @Test
-    public void javascript_executor_test1(){
+    public void javascript_executor_test1() {
         // TC #6: Scroll using JavascriptExecutor
         // 1- Open a chrome browser
         // 2- Go to: https://practice.cydeo.com/infinite_scroll
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll ");
 
         // 3- Use below JavaScript method and scroll
-        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
-        js.executeScript("window.scrollBy(0,0)");
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver(); // down-casting
 
         // a. 750 pixels down 10 times.
-
+        for (int i = 0; i < 10; i++) {
+            js.executeScript("window.scrollBy(0,750)");
+        }
 
         // b. 750 pixels up 10 times
+        for (int i = 0; i < 10; i++) {
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+
         // JavaScript method to use : window.scrollBy(0,0)
     }
 
