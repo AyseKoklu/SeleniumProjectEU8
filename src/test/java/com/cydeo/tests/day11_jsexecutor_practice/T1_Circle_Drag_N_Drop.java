451448c9d24830c098e2a40504dd9f07162cc5d4
AyseKoklu <rukoklu32@gmail.com>
2022-06-08 15:45:43 +0100

    day 11

--- src/test/java/com/cydeo/tests/day11_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -3,6 +3,8 @@ package com.cydeo.tests.day11_jsexecutor_practice;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -18,10 +20,17 @@ public class T1_Circle_Drag_N_Drop {
         acceptCookiesButton.click();
 
         // 2. Drag and drop the small circle to bigger circle.
+        WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
+        WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
 
+        Actions actions = new Actions(Driver.getDriver());
+        actions.dragAndDrop(smallCircle, bigCircle).perform();
 
         // 3. Assert:
         // -Text in big circle changed to: “You did great!”
+        String actualText = bigCircle.getText();
+        String expectedText = "You did great!";
+        Assert.assertEquals(actualText, expectedText);
     }
 
 }
